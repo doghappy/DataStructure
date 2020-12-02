0e8e8e34b740b8a5281f9fbc508773b4e524a21a
HeroWong <hero_wong@outlook.com>
2020-12-02 21:27:39 +0800

    线性表顺序存储结构

---
+++ DataStructure.Test/LinearList/LinearArrayListTest.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructure.LinearList;
+
+namespace DataStructure.Test.LinearList
+{
+    [TestClass]
+    public class LinearArrayListTest
+    {
+        [TestMethod]
+        public void TestInit()
+        {
+            var linearArrayList = new LinearArrayList<int>(20);
+            Assert.AreEqual(20, linearArrayList.MaxLength);
+            Assert.AreEqual(0, linearArrayList.Length);
+        }
+
+        [TestMethod]
+        public void Insert1Test()
+        {
+            var linearArrayList = new LinearArrayList<int>(20);
+            linearArrayList.Insert(0, 2);
+            Assert.AreEqual(1, linearArrayList.Length);
+            Assert.AreEqual(2, linearArrayList[0]);
+            Assert.AreEqual(20, linearArrayList.MaxLength);
+        }
+
+        [TestMethod]
+        public void Insert2Test()
+        {
+            var linearArrayList = new LinearArrayList<int>(20);
+            linearArrayList.Insert(0, 4);
+            linearArrayList.Insert(1, 5);
+            linearArrayList.Insert(2, 6);
+            Assert.AreEqual(3, linearArrayList.Length);
+            Assert.AreEqual(4, linearArrayList[0]);
+            Assert.AreEqual(5, linearArrayList[1]);
+            Assert.AreEqual(6, linearArrayList[2]);
+            Assert.AreEqual(20, linearArrayList.MaxLength);
+        }
+
+        [TestMethod]
+        public void Insert3Test()
+        {
+            var linearArrayList = new LinearArrayList<int>(20);
+            linearArrayList.Insert(0, 4);
+            linearArrayList.Insert(1, 5);
+            linearArrayList.Insert(1, 6);
+            Assert.AreEqual(3, linearArrayList.Length);
+            Assert.AreEqual(4, linearArrayList[0]);
+            Assert.AreEqual(6, linearArrayList[1]);
+            Assert.AreEqual(5, linearArrayList[2]);
+            Assert.AreEqual(20, linearArrayList.MaxLength);
+        }
+
+        [TestMethod]
+        public void Remove1Test()
+        {
+            var linearArrayList = new LinearArrayList<int>(20);
+            linearArrayList.Insert(0, 2);
+            linearArrayList.Remove(0);
+            Assert.AreEqual(0, linearArrayList.Length);
+            Assert.AreEqual(20, linearArrayList.MaxLength);
+        }
+
+        [TestMethod]
+        public void Remove2Test()
+        {
+            var linearArrayList = new LinearArrayList<int>(20);
+            linearArrayList.Insert(0, 4);
+            linearArrayList.Insert(1, 5);
+            linearArrayList.Insert(2, 6);
+            linearArrayList.Remove(1);
+            Assert.AreEqual(2, linearArrayList.Length);
+            Assert.AreEqual(4, linearArrayList[0]);
+            Assert.AreEqual(6, linearArrayList[1]);
+            Assert.AreEqual(20, linearArrayList.MaxLength);
+        }
+
+        [TestMethod]
+        public void Remove3Test()
+        {
+            var linearArrayList = new LinearArrayList<int>(20);
+            linearArrayList.Insert(0, 4);
+            linearArrayList.Insert(1, 5);
+            linearArrayList.Insert(2, 6);
+            linearArrayList.Remove(2);
+            Assert.AreEqual(2, linearArrayList.Length);
+            Assert.AreEqual(4, linearArrayList[0]);
+            Assert.AreEqual(5, linearArrayList[1]);
+            Assert.AreEqual(20, linearArrayList.MaxLength);
+        }
+    }
+}
---
+++ DataStructure/LinearList/LinearArrayList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace DataStructure.LinearList
+{
+    public class LinearArrayList<T>
+    {
+        public LinearArrayList(int maxLength)
+        {
+            MaxLength = maxLength;
+            Data = new T[MaxLength];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index > Length - 1)
+                    throw new IndexOutOfRangeException();
+                else
+                    return Data[index];
+            }
+            //set => Data[index] = value;
+        }
+
+        private T[] Data { get; }
+
+        public int Length { get; private set; }
+        public int MaxLength { get; }
+
+
+        public void Insert(int index, T element)
+        {
+            if (Length == MaxLength || index < 0 || index > Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            for (int i = Length - 1; i >= index; i--)
+            {
+                Data[i + 1] = Data[i];
+            }
+            Data[index] = element;
+            Length++;
+        }
+
+        public void Remove(int index)
+        {
+            if (Length == 0 || index < 0 || index > Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index < Length - 1)
+            {
+                for (int i = index + 1; i < Length; i++)
+                {
+                    Data[i - 1] = Data[i];
+                }
+            }
+            Length--;
+        }
+    }
+}
