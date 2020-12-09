43049bd0cc4ec8a852975499a3f9c88618f1222c
HeroWong <hero_wong@outlook.com>
2020-12-09 22:50:43 +0800

    栈

---
+++ DataStructure/Stack/MyLinkStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Stack
+{
+    public class MyLinkStackNode<T>
+    {
+        public T Data { get; set; }
+        public MyLinkStackNode<T> Next { get; set; }
+    }
+
+    public class MyLinkStack<T>
+    {
+        public MyLinkStackNode<T> Top { get; private set; }
+        public int Count { get; private set; }
+
+        public void Push(T e)
+        {
+            var node = new MyLinkStackNode<T>
+            {
+                Data = e
+            };
+            node.Next = Top;
+            Top = node;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (Top == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            var e = Top.Data;
+            Top = Top.Next;
+            return e;
+        }
+    }
+}
---
+++ DataStructure/Stack/MyStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Stack
+{
+    public class MyStack<T>
+    {
+        public MyStack(int maxSize)
+        {
+            Data = new T[maxSize];
+            Top = -1;
+        }
+
+        private T[] Data { get; }
+        public int Top { get; private set; }
+
+        public void Push(T element)
+        {
+            if (Top >= Data.Length - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            Top++;
+            Data[Top] = element;
+        }
+
+        public T Pop()
+        {
+            if (Top == -1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            var e = Data[Top];
+            Top--;
+            return e;
+        }
+    }
+}
